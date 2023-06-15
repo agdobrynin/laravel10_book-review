66795f026645ae1169cb94aeff4d6bf99182b881
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-06-15 18:27:48 +0400

    dev: Model factory. Db seeders.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Book;
+use App\Models\Review;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        Book::factory(30)
+            ->has(
+                Review::factory(
+                    rand(10, 20),
+                    function (array $attr, Book $book) {
+                        $date = fake()->dateTimeBetween($book->created_at);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+                        return [
+                            'created_at' => $date,
+                            'updated_at' => $date,
+                        ];
+                    }
+                )
+            )
+            ->create();
     }
 }
