9b1f0256238630a27a2dbc7f8374267488a40685
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-06-15 18:04:33 +0400

    feat: Models "Book", "Review" with migrations.

---
+++ database/migrations/2023_06_15_120317_create_reviews_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Book;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->timestamps();
+
+            $table->text('content');
+            $table->unsignedTinyInteger('rating');
+
+            $table->foreignIdFor(Book::class)
+                ->constrained()
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
