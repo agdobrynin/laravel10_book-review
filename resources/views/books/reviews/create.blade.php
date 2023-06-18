f00e5cdefe1d25d6d3fb319ebc6c156df2c67a6b
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-06-18 20:24:15 +0400

    feat: Add to template button "back to book" and "books list"

--- resources/views/books/reviews/create.blade.php
+++ resources/views/books/reviews/create.blade.php
@@ -1,6 +1,7 @@
 <x-layout :pageTitle="'Add new review for' . $book['title']">
-    <div class="mb-4">
+    <div class="mb-4 flex justify-between">
         <a class="btn" href="{{ route('books.show', $book) }}">Back to book</a>
+        <a class="btn" href="{{ route('books.index') }}">Books list</a>
     </div>
     <h2 class="mb-4 text-xl font-semibold">Add new review for book &laquo;{{ $book->title }}&raquo;</h2>
 
@@ -12,14 +13,16 @@
                 @class(['input h-28', 'input-error' => $errors->has('content')])
                 >{{ old('content') }}</textarea>
             @error('content')
-                <div class="error"> {{ $message }} </div>
+            <div class="error"> {{ $message }} </div>
             @enderror
         </div>
         <div class="mb-4">
             <label for="rating" class="text-slate-400 block mb-2">Rating</label>
             <select name="rating" id="rating" @class(['input', 'input-error' => $errors->has('rating')])>
                 <option value="">Select a Rating</option>
-                @for($i = 1; $i <= 5; $i++) <option value="{{ $i }}"> {{ $i }}</option> @endfor
+                @for($i = 1; $i <= 5; $i++)
+                    <option value="{{ $i }}"> {{ $i }}</option>
+                @endfor
             </select>
             @error('rating')
             <div class="error"> {{ $message }} </div>
@@ -27,7 +30,9 @@
         </div>
         <div class="mb-4 flex justify-between">
             <div><a href="{{ route('books.show', $book) }}" class="btn h-10">Cancel</a></div>
-            <div><button type="submit" class="btn h-10"><span class="text-indigo-600">Save</span></button></div>
+            <div>
+                <button type="submit" class="btn h-10"><span class="text-indigo-600">Save</span></button>
+            </div>
         </div>
     </form>
 </x-layout>
